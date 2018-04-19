33aa673a7dcfc329ac64635b76b743528fb21dd1
Sigfrido Gomez <Sigfrido@sigfridos-mbp.attlocal.net>
2018-04-18 23:47:46 -0500

    added a func chart
    --added a functional chart with our VARS
    -- started to tweek some elements
    -- reopened main.js to add fionality
    
    task to be done
    -- need to add visual coordinate efforts

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ function createMap(){
 
 
         //adding csv values to the chart
-        setChart(csvMaster);
+        createChart(csvMaster);
         
         var groupedOverlays = {
           "Tract Data Overlays": tractLayers,
@@ -90,6 +90,7 @@ function createMap(){
 
 // source:http://leafletjs.com/examples/choropleth/
 function changeLegend(layer,tractScales,map){
+    //lol, MTA is NYC
     var MTA = ['MTA "L" Routes','MTA "L" Stations','New Buildings Since 2010']
     var expressed = layer.name;
     
@@ -522,115 +523,428 @@ function smoothScroll(){
   });
 }; // end of smoothScroll
 
-//_________________________________
-//Start of CHART with global variables
+//_______________________________________________________________________________________________________________
+//_______________________________________________________________________________________________________________
+//_______________________________________________________________________________________________________________
+//_______________________________________________________________________________________________________________
+//_______________________________________________________________________________________________________________
 
+//createCHART mofo 
+//https://bl.ocks.org/syntagmatic/05a5b0897a48890133beb59c815bd953
+function createChart(csvMaster){
 
+    var screenHeight = $(window).height(),
+        parentWidth = $("#data").parent().width(),
+        margin = {top: 66, right: 110, bottom: 20, left: 188},
+        height = (screenHeight*.88) - margin.top - margin.bottom,
+        width = (parentWidth*1) - margin.left - margin.right,
+        innerHeight = height - 2;
 
-//margin and following default settings but will need to tweek 
-//global VAR
-//updated 20180416
-var margin = {top: 30, right: 10, bottom: 10, left: 10},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-//scalePoint
-var xScale = d3.scaleLinear([0, width], 1),
-    yScale = {},
-    dragging = {};
+    var devicePixelRatio = window.devicePixelRatio || 1;
 
-var line = d3.line(),
-    yAxis = d3.axisLeft()
-        .scale(yScale),
-    background,
-    foreground;
+    var color = d3.scaleOrdinal()
+    .range(
+        ["#5DA5B3",
+        "#D58323",
+        "#DD6CA7",
+        "#54AF52",
+        "#8C92E8",
+        "#E15E5A",
+        "#725D82",
+        "#776327",
+        "#50AB84",
+        "#954D56",
+        "#AB9C27",
+        "#517C3F",
+        "#9D5130",
+        "#357468",
+        "#5E9ACF",
+        "#C47DCB",
+        "#7D9E33",
+        "#DB7F85",
+        "#BA89AD",
+        "#4C6C86",
+        "#B59248",
+        "#D8597D",
+        "#944F7E",
+        "#D67D4B",
+        "#8F86C2"]);
+    // these are assgined to the primary key and will need to be changed
+    //for now they serve as a beautiful mess to the chart. 
 
-var svg = d3.select("#dataarea").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    //number acutally is a float too 
+    var types = {
+      "Number": {
+        key: "Number",
+        coerce: function(d) { return +d; },
+        extent: d3.extent,
+        within: function(d, extent, dim) { return extent[0] <= dim.scale(d) && dim.scale(d) <= extent[1]; },
+        defaultScale: d3.scaleLinear().range([innerHeight, 0])
+      },
+      "String": {
+        key: "String",
+        coerce: String,
+        extent: function (data) { return data.sort(); },
+        within: function(d, extent, dim) { return extent[0] <= dim.scale(d) && dim.scale(d) <= extent[1]; },
+        defaultScale: d3.scalePoint().range([0, innerHeight])
+      },
+      //I think they anticipated it but never used
+      // can be left out for the transformation
+      // i'll leave it for now 
+      //honestly don't think we'll need it...
+      "Date": {
+        key: "Date",
+        coerce: function(d) { return new Date(d); },
+        extent: d3.extent,
+        within: function(d, extent, dim) { return extent[0] <= dim.scale(d) && dim.scale(d) <= extent[1]; },
+        defaultScale: d3.scaleTime().range([0, innerHeight])
+      }
+    }; // end of var types 
 
-// div ID for html = chartData
+    //make up the columns in the chart 
+    //first column all the way to the end
+    var dimensions = [
+      {
+        key: "CG_GEOID",
+        description: "Key Geo-ID",
+        type: types["String"],
+        axis: d3.axisLeft()
+          .tickFormat(function(d,i) {
+            return d;
+          })
+      },
+      {
+        key: "GENT_IDX",
+        description: "Gentrification Index",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      {
+        key: "PCIPCTCHG",
+        description: "Per Capita Income Growth (%)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      {
+        key: "POVPCTCHG",
+        description: "Poverty Rate Growth (% Difference)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
 
-function setChart(csvMaster){
+      {
+        key: "POPPCTCHG",
+        description: "Population Growth (%)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      {
+        key: "BLKPCTCHG",
+        description: "Black Pop Growth (%)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
 
-  // Extract the list of dimensions and create a scale for each.
-  //
-  xScale.domain(dimensions = d3.keys(csvMaster[0]).filter(function(d) {
-    return d != "name" && (yScale[d] = d3.scaleLinear()
-        .domain(d3.extent(csvMaster, function(p) { return +p[d]; }))
-        .range([height, 0]));
-  }));
+     {
+        key: "ASNPCTCHG",
+        description: "Asian Pop Growth (%)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
 
-  // Add grey background lines for context.
-  background = svg.append("g")
-      .attr("class", "background")
-    .selectAll("path")
-      .data(csvMaster)
-    .enter().append("path")
-      .attr("d", path);
+      {
+        key: "POPPCTCHG",
+        description: "Population Growth (%)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      {
+        key: "HSPPCTCHG",
+        description: "Hispanic Pop Growth (%)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      {
+        key: "WHTPCTCHG",
+        description: "White Pop Growth (%)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      {
+        key: "HSPCTCHG",
+        description: "High School or Less Growth (% Difference)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+     {
+        key: "PHSPCTCHG",
+        description: "At least Some College Growth (% Difference)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      {
+        key: "ORRPCTCHG",
+        description: "Renter:Owner Ratio Growth (%)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      {
+        key: "HSPPCTCHG",
+        description: "Hispanic Pop Growth (%)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      {
+        key: "UERPCTCHG",
+        description: "Unemployment Rate Growth (% Difference)",
+        type: types["Number"],
+        scale: d3.scaleSqrt().range([innerHeight, 0])
+      },
+      /*
+      //only keeping this around because it shows the build of a right axis
+      {
+        key: "manufac_name",
+        description: "Manufacturer", 
+        type: types["String"],
+        axis: d3.axisRight()
+          .tickFormat(function(d,i) {
+            if (d == null) return "(null)";
+            return i % 5 == 0 ? d.slice(0,22) : "";
+          })
+          
+      } 
+      */
+    ];
 
-  // Add blue foreground lines for focus.
-  foreground = svg.append("g")
-      .attr("class", "foreground")
-    .selectAll("path")
-      .data(csvMaster)
-    .enter().append("path")
-      .attr("d", path);
+    var xscale = d3.scalePoint()
+        .domain(d3.range(dimensions.length))
+        .range([0, width]);
 
-  // Add a group element for each dimension.
-  var g = svg.selectAll(".dimension")
-      .data(dimensions)
-    .enter().append("g")
-      .attr("class", "dimension")
-      .attr("transform", function(d) { return "translate(" + xScale(d) + ")"; })
-      .attr("drag")
-      .call(d3.behavior.drag()
-        .origin(function(d) { return {xScale: x(d)}; })
-        .on("dragstart", function(d) {
-          dragging[d] = xScale (d);
-          background.attr("visibility", "hidden");
-        })
-        .on("drag", function(d) {
-          dragging[d] = Math.min(width, Math.max(0, d3.event.xScale ));
-          foreground.attr("d", path);
-          dimensions.sort(function(a, b) { return position(a) - position(b); });
-          xScale .domain(dimensions);
-          g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
-        })
-        .on("dragend", function(d) {
-          delete dragging[d];
-          transition(d3.select(this)).attr("transform", "translate(" + xScale (d) + ")");
-          transition(foreground).attr("d", path);
-          background
-              .attr("d", path)
-            .transition()
-              .delay(500)
-              .duration(0)
-              .attr("visibility", null);
-        }));
+    var yAxis = d3.axisLeft();
 
-  // Add an axis and title.
-  g.append("g")
-      .attr("class", "axis")
-      .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
-    .append("text")
-      .style("text-anchor", "middle")
-      .attr("yScale", -9)
-      .text(function(d) { return d; });
+    var container = d3.select("#data").append("div")
+        .attr("class", "parcoords")
+        .style("width", width + margin.left + margin.right + "px")
+        .style("height", height + margin.top + margin.bottom + "px");
 
-  // Add and store a brush for each axis.
-  g.append("g")
-      .attr("class", "brush")
-      .each(function(d) {
-        d3.select(this).call(yScale[d].brush = d3.svg.brush().y(yScale[d]).on("brushstart", brushstart).on("brush", brush));
-      })
-    .selectAll("rect")
-      .attr("xScale ", -8)
-      .attr("width", 16);
+    var svg = container.append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+/*
+    canvas is an HTML element like the div with canvas you can 
+    fill pixels the result is a practical png image. big advantage is that 
+    canvas can create thousands of separate elements without significantly effecting performance
+    */
+    var canvas = container.append("canvas")
+        .attr("width", width * devicePixelRatio)
+        .attr("height", height * devicePixelRatio)
+        .style("width", width + "px")
+        .style("height", height + "px")
+        .style("margin-top", margin.top + "px")
+        .style("margin-left", margin.left + "px");
+    /*
+    canvas is actually a DOM element 
+    */
+    var ctx = canvas.node().getContext("2d");
+    ctx.globalCompositeOperation = 'darken';
+    ctx.globalAlpha = 0.15;
+    ctx.lineWidth = 1.5;
+    ctx.scale(devicePixelRatio, devicePixelRatio);
 
-}; // end of setChart
+    var axes = svg.selectAll(".axis")
+        .data(dimensions)
+      .enter().append("g")
+        .attr("class", function(d) { return "axis " + d.key.replace(/ /g, "_"); })
+        .attr("transform", function(d,i) { return "translate(" + xscale(i) + ")"; });
+    //everything above is a csv. pre-data load so this needs to be prep. for data ingestion    
+
+    //start of loading data and of VIZ
+    d3.csv("data/master.csv", function(error, data) {
+      if (error) throw error;
+
+      // shuffle the data!
+      data = d3.shuffle(data);
+
+      data.forEach(function(d) {
+        dimensions.forEach(function(p) {
+          d[p.key] = !d[p.key] ? null : p.type.coerce(d[p.key]);
+        });
+
+        // truncate long text strings to fit in data table
+        for (var key in d) {
+          if (d[key] && d[key].length > 35) d[key] = d[key].slice(0,36);
+        }
+      });
+
+      // type/dimension default setting happens here
+      dimensions.forEach(function(dim) {
+        if (!("domain" in dim)) {
+          // detect domain using dimension type's extent function
+          dim.domain = d3_functor(dim.type.extent)(data.map(function(d) { return d[dim.key]; }));
+        }
+        if (!("scale" in dim)) {
+          // use type's default scale for dimension
+          dim.scale = dim.type.defaultScale.copy();
+        }
+        dim.scale.domain(dim.domain);
+      });
+
+      var render = renderQueue(draw).rate(50);
+
+      ctx.clearRect(0,0,width,height);
+      ctx.globalAlpha = d3.min([0.85/Math.pow(data.length,0.3),1]);
+      render(data);
+
+      axes.append("g")
+          .each(function(d) {
+            var renderAxis = "axis" in d
+              ? d.axis.scale(d.scale)  // custom axis
+              : yAxis.scale(d.scale);  // default axis
+            d3.select(this).call(renderAxis);
+          })
+        .append("text")
+          .attr("class", "title")
+          .attr("text-anchor", "start")
+          .text(function(d) { return "description" in d ? d.description : d.key; });
+
+      // Add and store a brush for each axis.
+      axes.append("g")
+          .attr("class", "brush")
+          .each(function(d) {
+            d3.select(this).call(d.brush = d3.brushY()
+              .extent([[-10,0], [10,height]])
+              .on("start", brushstart)
+              .on("brush", brush)
+              .on("end", brush)
+            )
+          })
+        .selectAll("rect")
+          .attr("x", -8)
+          .attr("width", 16);
+
+      d3.selectAll(".axis.CG_GEOID .tick text")
+        .style("fill", color);
+
+      function project(d) {
+        return dimensions.map(function(p,i) {
+          // check if data element has property and contains a value
+          if (
+            !(p.key in d) ||
+            d[p.key] === null
+          ) return null;
+
+          return [xscale(i),p.scale(d[p.key])];
+        });
+      }; // end of project function
+
+      function draw(d) {
+        ctx.strokeStyle = color(d.CG_GEOID);
+        ctx.beginPath();
+        var coords = project(d);
+        coords.forEach(function(p,i) {
+          // this tricky bit avoids rendering null values as 0
+          if (p === null) {
+            // this bit renders horizontal lines on the previous/next
+            // dimensions, so that sandwiched null values are visible
+            if (i > 0) {
+              var prev = coords[i-1];
+              if (prev !== null) {
+                ctx.moveTo(prev[0],prev[1]);
+                ctx.lineTo(prev[0]+6,prev[1]);
+              }
+            }
+            if (i < coords.length-1) {
+              var next = coords[i+1];
+              if (next !== null) {
+                ctx.moveTo(next[0]-6,next[1]);
+              }
+            }
+            return;
+          }
+
+          if (i == 0) {
+            ctx.moveTo(p[0],p[1]);
+            return;
+          }
+
+          ctx.lineTo(p[0],p[1]);
+        });
+        ctx.stroke();
+      } // end of draw function 
+
+      function brushstart() {
+        d3.event.sourceEvent.stopPropagation();
+      }
+
+      // Handles a brush event, toggling the display of foreground lines.
+      function brush() {
+        render.invalidate();
+
+        var actives = [];
+        svg.selectAll(".axis .brush")
+          .filter(function(d) {
+            return d3.brushSelection(this);
+          })
+          .each(function(d) {
+            actives.push({
+              dimension: d,
+              extent: d3.brushSelection(this)
+            });
+          });
+
+        var selected = data.filter(function(d) {
+          if (actives.every(function(active) {
+              var dim = active.dimension;
+              // test if point is within extents for each active brush
+              return dim.type.within(d[dim.key], active.extent, dim);
+            })) {
+            return true;
+          }
+        });
+
+        // show ticks for active brush dimensions
+        // and filter ticks to only those within brush extents
+        /*
+        svg.selectAll(".axis")
+            .filter(function(d) {
+              return actives.indexOf(d) > -1 ? true : false;
+            })
+            .classed("active", true)
+            .each(function(dimension, i) {
+              var extent = extents[i];
+              d3.select(this)
+                .selectAll(".tick text")
+                .style("display", function(d) {
+                  var value = dimension.type.coerce(d);
+                  return dimension.type.within(value, extent, dimension) ? null : "none";
+                });
+            });
+
+        // reset dimensions without active brushes
+        svg.selectAll(".axis")
+            .filter(function(d) {
+              return actives.indexOf(d) > -1 ? false : true;
+            })
+            .classed("active", false)
+            .selectAll(".tick text")
+              .style("display", null);
+        */
+
+        ctx.clearRect(0,0,width,height);
+        ctx.globalAlpha = d3.min([0.85/Math.pow(selected.length,0.3),1]);
+        render(selected);
+
+      }
+    });
+
+    function d3_functor(v) {
+      return typeof v === "function" ? v : function() { return v; };
+    };
+    
+}; // end of createChart
 
 function position(d) {
   var v = dragging[d];
@@ -659,7 +973,7 @@ function brush() {
       return extents[i][0] <= d[p] && d[p] <= extents[i][1];
     }) ? null : "none";
   });
-}
+} // end of brush
 
 
 
